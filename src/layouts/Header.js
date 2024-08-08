d7bfdd038ceee56d6b2a59ca3b96d770eb06d274
jaejin <jaejin0109@gmail.com>
2024-08-08 02:11:34 -0500

    added viewport react library

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,37 +1,48 @@
 import { Link } from 'react-router-dom';
-import { AppBar, Container, Toolbar, Typography, Box, Button } from "@mui/material";
+import { AppBar, Container, Toolbar, Typography, Box, Button, Slide, useScrollTrigger } from "@mui/material";
 
 function Header() {
     return (
-        <AppBar position='sticky' sx={{ marginBottom: 4 }}>
-            <Container>
-                <Toolbar>
-                    <Typography
-                        variant='h6'
-                        component='div'
-                        sx={{ flexGrow: 3, display: { xs: 'none', sm: 'block', color: '#fff' } }}
-                    >
-                        <Button
-                            component={Link} to='/' sx={{ color: '#fff'}} disableRipple>
-                            Jaejin
+        <HideOnScroll>
+            <AppBar position='sticky' sx={{ marginBottom: 4 }}>
+                <Container>
+                    <Toolbar sx={{ display: 'flex' }}>
+                        <Button component={Link} to='/' sx={{ flexGrow: 3, display: { xs: 'none', sm: 'block', color: '#fff' }}} disableRipple>
+                            <Typography sx={{ fontFamily: 'Monospace', letterSpacing: 4 }}>Jaejin</Typography>
                         </Button>
-                    </Typography>
-                    <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
-                        <Button component={Link} to='/' sx={{ color: '#fff'}}>About Me</Button>
-                    </Box>
-                    <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
-                        <Button component={Link} to='/education' sx={{ color: '#fff'}}>Education</Button>
-                    </Box>
-                    <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
-                        <Button component={Link} to='/experience' sx={{ color: '#fff'}}>Experience</Button>
-                    </Box>
-                    <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
-                        <Button component={Link} to='/projects' sx={{ color: '#fff'}}>Projects</Button>
-                    </Box>
-                </Toolbar>
-            </Container>
-        </AppBar>
+                        <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+                            <Button component={Link} to='/' sx={{ color: '#fff'}}>About Me</Button>
+                        </Box>
+                        <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+                            <Button component={Link} to='/education' sx={{ color: '#fff'}}>Education</Button>
+                        </Box>
+                        <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+                            <Button component={Link} to='/experience' sx={{ color: '#fff'}}>Experience</Button>
+                        </Box>
+                        <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+                            <Button component={Link} to='/projects' sx={{ color: '#fff'}}>Projects</Button>
+                        </Box>
+                    </Toolbar>
+                </Container>
+            </AppBar>
+        </HideOnScroll>
     );
 }
+
+function HideOnScroll(props) {
+    const { children, window } = props;
+    // Note that you normally won't need to set the window ref as useScrollTrigger
+    // will default to window.
+    // This is only being set here because the demo is in an iframe.
+    const trigger = useScrollTrigger({
+      target: window ? window() : undefined,
+    });
+  
+    return (
+      <Slide appear={false} direction="down" in={!trigger}>
+        {children}
+      </Slide>
+    );
+  }
 
 export default Header;
